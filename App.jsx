ca4206f67989b9503550cf72eeebb82eabd3a4c8
Aman Soni <amansoni78695@gmail.com>
2024-07-12 17:06:32 +0530

    git setup

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import StartQuiz from './components/Screens/startQuiz'
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+      {/* <StartQuiz/> */}
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
